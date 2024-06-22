1c08f077321ca431453eb188cd7b9a0a1bc53d80
kch1015 <songwoo1015@gmail.com>
2024-06-23 02:21:34 +0900

    feat: DB 연결, User 엔티티 추가

---
+++ src/main/java/com/study/everytime/domain/user/entity/User.java
@@ -0,0 +1,36 @@
+package com.study.everytime.domain.user.entity;
+
+import com.study.everytime.global.auth.dto.Provider;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = @Index(name = "idx__provider__sub", columnList = "provider, sub"))
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String username;
+    private String email;
+
+    private Provider provider;
+    private String sub;
+
+    private User(String username, String email, Provider provider, String sub) {
+        this.username = username;
+        this.email = email;
+        this.provider = provider;
+        this.sub = sub;
+    }
+
+    public static User of(String username, String email, Provider provider, String sub) {
+        return new User(username, email, provider, sub);
+    }
+
+}
