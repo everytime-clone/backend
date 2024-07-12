e13422a04b425ccfe56bb4ae6dcd0b90fcb267de
kch1015 <songwoo1015@gmail.com>
2024-07-13 00:00:10 +0900

    feat: createdBy, modifiedBy Auditing 기능 추가
    Security 인증 정보에서 userId 획득하여 기록
    JpaConfig 분리

--- src/main/java/com/study/everytime/EverytimeApplication.java
+++ src/main/java/com/study/everytime/EverytimeApplication.java
@@ -3,11 +3,9 @@ package com.study.everytime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableJpaAuditing
 public class EverytimeApplication {
 
     public static void main(String[] args) {
